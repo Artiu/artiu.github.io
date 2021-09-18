a007db5a0a8469c5f43c6a4e6d92fe54bda3ab28
Artiu <71273256+Artiu@users.noreply.github.com>
2021-09-19 00:19:19 +0200

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -2,20 +2,35 @@ import styled from "styled-components"
 
 const Div = styled.div`
     scroll-snap-align: start;
-    background-color: lightblue;
+    background-color: #48cae4;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 30px;
+    color: black;
+    gap: 10px;
+    a{
+        display: flex;
+        align-items: center;
+    }
     img {
         width: 50px;
     }
 `
-
+const Header = styled.h2`
+    width: 100%;
+    text-align: center;
+`
 export default function Footer() {
     return (
         <Div>
+            <Header>Contact me</Header>
+            <a href="mailto:oborskiartur123@gmail.com">
+                Email: oborskiartur123@gmail.com
+            </a>
             <a href="https://github.com/Artiu">
                 <img src="/github.png" alt="github logo"/>
-            </a>
-            <a href="mailto:oborskiartur123@gmail.com">
-                Email
             </a>
         </Div>
     )
