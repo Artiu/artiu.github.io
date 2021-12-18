0441d5642f00825a7697c863adc9c997efb03c44
Artiu <71273256+Artiu@users.noreply.github.com>
2021-12-18 11:28:16 +0100

    Fix for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 module.exports = {
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+            hair: "#92816d",
+            skin: "#e8beac",
+            eye: "#85abce",
+            "eye-border": "#111111",
+        },
         extend: {
             gridTemplateColumns: {
-                "15": "repeat(15, 1fr)"
+                15: "repeat(15, 1fr)",
             },
             gridTemplateRows: {
-                "20": "repeat(20, 1fr)"
+                20: "repeat(20, 1fr)",
             },
             width: {
-                "40": "150px"
+                40: "150px",
             },
             height: {
-                "50": "200px"
+                50: "200px",
             },
         },
     },
