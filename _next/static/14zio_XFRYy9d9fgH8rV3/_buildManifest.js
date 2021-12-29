23e5c8f7964b20b07955ea2bdde7170220b3c4cf
Artiu <71273256+Artiu@users.noreply.github.com>
2021-12-29 19:53:13 +0100

    Updates

---
+++ _next/static/14zio_XFRYy9d9fgH8rV3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d12db3e5b23841b7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
